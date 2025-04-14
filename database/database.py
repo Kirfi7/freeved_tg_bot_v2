9bc8075c6872ff00835cb3f29fbeb2f440220b81
Kirfi <kirbel357@gmail.com>
2025-04-14 15:00:02 +0500

    feat: update

---
+++ database/database.py
@@ -0,0 +1,33 @@
+import sys
+import pymongo
+
+# Подключение к MongoDBf
+uri = f"mongodb://127.0.0.1:12341/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5"
+try:
+    client = pymongo.MongoClient(uri)
+    print("Соединение с БД установлено успешно!")
+except pymongo.errors.ConfigurationError:
+    print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
+    sys.exit(1)
+
+db = client.myDatabase
+
+collection_messages = "messages"
+if collection_messages not in db.list_collection_names():
+    db.create_collection(collection_messages)
+messages_collection = db[collection_messages]
+
+collection_admins = "admins"
+if collection_admins not in db.list_collection_names():
+    db.create_collection(collection_admins)
+admins_collection = db[collection_admins]
+
+collection_blacklist = "blacklist"
+if collection_blacklist not in db.list_collection_names():
+    db.create_collection(collection_blacklist)
+blacklist_collection = db[collection_blacklist]
+
+collection_waiting = "waiting"
+if collection_waiting not in db.list_collection_names():
+    db.create_collection(collection_waiting)
+waiting_collection = db[collection_waiting]
