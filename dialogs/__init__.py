7a297f8369e6c52d6b9b41b6d819976b52842069
Mikhail <mialal0505@gmail.com>
2025-04-19 09:44:15 +0300

    Upd

---
+++ dialogs/__init__.py
@@ -0,0 +1,5 @@
+from aiogram import Router
+
+...
+
+base_router = Router()
--- main.py
+++ main.py
@@ -4,25 +4,14 @@ import config
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-# from filters.filters import IsNotBannedCB, IsNotBannedMSG
-# from handlers import (
-#     bot_messager,
-#     admin_panel,
-#     main_dialog,
-#     comment_tracker,
-# )
+from dialogs import base_router
 
 
 async def main():
     bot = Bot(token=config.TOKEN, parse_mode='HTML')
     dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
-    # dp.include_routers(
-    #     comment_tracker.router,
-    #     admin_panel.router,
-    #     main_dialog.router,
-    #     bot_messager.router,
-    # )
+    dp.include_router(base_router)
 
     # dp.message.filter(IsNotBannedMSG())
     # dp.callback_query.filter(IsNotBannedCB())
