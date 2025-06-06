f6ac9bb218c9e74429cb0fd5e9a40555ef75cb60
MarsiFrilex <tolmachev.gleb@mail.ru>
2025-06-06 13:21:30 +0300

    Fix id

--- database/services/posts.py
+++ database/services/posts.py
@@ -7,7 +7,8 @@ class PostsDB:
     def init_post(data: PostInit):
         post_id: int = 1
         if post := posts_collection.find_one({}, {"sort": {"id": -1}}):
-            post_id = post.get("id") or 1
+            if id := post.get("id"):
+                post_id = id
         insert_data = Post(**data.model_dump(), id=post_id)
         posts_collection.insert_one(insert_data.model_dump())
         return post_id
