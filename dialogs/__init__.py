12429d6379fc8988b5c489cc83d372f3fd532777
MarsiFrilex <tolmachev.gleb@mail.ru>
2025-06-07 15:18:06 +0300

    Fix

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -17,5 +17,5 @@ async def start_bot(message: Message):
 
 
 base_router.include_router(channel_posting)
-base_router.include_router(admin_conversation)
 base_router.include_router(admin_choices)
+base_router.include_router(admin_conversation)
