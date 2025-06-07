ddff650a0a9761c4b3983c67a3629f1fac5b9c56
MarsiFrilex <tolmachev.gleb@mail.ru>
2025-06-07 15:05:29 +0300

    Init conversation

--- dialogs/admin_conversation.py
+++ dialogs/admin_conversation.py
@@ -1,11 +1,24 @@
 from aiogram import F, Router
-from aiogram.types import CallbackQuery
-
-...
+from aiogram.types import Message, CallbackQuery
 
 router = Router()
 
 
-@router.callback_query(F.callback_data == "k")
+@router.callback_query(F.callback_data == "conversation")
 async def init_conversation(callback: CallbackQuery):
+    ...
+
+
+@router.message()
+async def handle_message(message: Message):
+    ...
+
+
+@router.callback_query()
+async def reply_to_message(callback: CallbackQuery):
+    ...
+
+
+@router.message()
+async def handle_reply(message: Message):
     ...
