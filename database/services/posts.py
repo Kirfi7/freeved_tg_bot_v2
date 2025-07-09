3a556b2930fffedc522b9c597b84a06e4646581c
Mikhail <im.alekhin@yandex.ru>
2025-07-09 19:52:33 +0300

    Debug 2

--- database/services/posts.py
+++ database/services/posts.py
@@ -1,3 +1,5 @@
+import pymongo
+
 from database.database import posts_collection
 from database.models import Post, PostInit
 
@@ -6,11 +8,11 @@ class PostsDB:
     @staticmethod
     def init_post(data: PostInit):
         post_id: int = 1
-        if post := posts_collection.find_one({}, {"sort": {"id": -1}}):
-            print('post', post)
-            if id := post.get("id"):
-                print('id', id)
-                post_id = id + 1
+        posts = posts_collection.find().sort('id', pymongo.ASCENDING)
+        print('posts', posts)
+
+        if posts:
+            post_id = posts[0]['id'] + 1
         insert_data = Post(**data.model_dump(), id=post_id)
         posts_collection.insert_one(insert_data.model_dump())
         return post_id
