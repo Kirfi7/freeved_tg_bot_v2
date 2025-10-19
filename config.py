6c059a1becf5d44fe63b54d033ead09c7c829b0b
daniil <dkrapivin.04@gmail.com>
2025-10-19 21:33:20 +0500

    конфиг

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
-# TOKEN: str = "7742068663:AAH-xi3TdKyC3467HNJy6bFLVOEHOT45jWk"
-TOKEN: str = "8478992235:AAFsKKpCpfNqqsQrxbUj_J3JguSMgNmG77I"
-# CHANNEL: int = -1002542074756
-CHANNEL: int = -1001918020494
-# GROUP: int = -1002653430202
-GROUP: int = -1001926536763
-# ADMIN: int = 777198928
-ADMIN: int = 107953601
+DEBUG: bool = True
 
-# Коротко: это “короткий” ID канала для ссылок вида https://t.me/c/<id>/<msg_id>. Он берётся из chat_id канала,
-# просто без префикса -100.
-# LINK_ID: int = 2542074756
-LINK_ID: int = 1918020494
+if DEBUG:
+    # тестовый
+    TOKEN: str = "7742068663:AAH-xi3TdKyC3467HNJy6bFLVOEHOT45jWk"
+    CHANNEL: int = -1002542074756
+    GROUP: int = -1002653430202
+    ADMIN: int = 777198928
+    # Коротко: это “короткий” ID канала для ссылок вида https://t.me/c/<id>/<msg_id>. Он берётся из chat_id канала,
+    # просто без префикса -100.
+    LINK_ID: int = 2542074756
+else:
+    # прод
+    TOKEN: str = "8478992235:AAFsKKpCpfNqqsQrxbUj_J3JguSMgNmG77I"
+    CHANNEL: int = -1001918020494
+    GROUP: int = -1001926536763
+    ADMIN: int = 107953601
+    LINK_ID: int = 1918020494
 
-# ADMIN_ID: int = 107953601
